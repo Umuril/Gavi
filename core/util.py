88e6617b86e2aab127e2a801ae53d31deafe3c12
Carlo Nonato <carlo.nonato95@gmail.com>
2017-11-25 00:30:03 +0100

    Scrive i tweets su file di testo

--- core/util.py
+++ core/util.py
@@ -1,7 +1,7 @@
 def write_tweets(tweets, filename):
     with open(filename, 'w') as out_file:
         for tweet in tweets:
-            for key, value in tweet.items():
-                out_file.write(key + ' : ' + value + '\n')
+            for field, content in tweet:
+                out_file.write(field + ' : ' + content + '\n')
             out_file.write('\n')
 
